212230e979e7fbbefddf54aa9332148ad75473db
Tom Macklam <tom_macklam@hotmail.com>
2017-03-27 17:02:39 +0100

    changes

--- myJS.js
+++ myJS.js
@@ -7,7 +7,7 @@ function checkElementExists(elem){
     } else return true;
 }
 
-function dateTime(){
+function changeDateTime(){
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth()+1;
@@ -32,9 +32,64 @@ function dateTime(){
         s='0'+s
     }
 
+    
+    if(localStorage.getItem("clockSettings") == "12H"){
+        var h = today.getHours() % 12 || 12;
+    }
+
     today = dd+'/'+mm+'/'+yyyy+' '+h+':'+m+':'+s;
     if(checkElementExists('#dateTime')){
         document.getElementById('dateTime').innerHTML = today;
     }
 }
-setInterval(dateTime, 1000);
+setInterval(changeDateTime, 1000);
+
+function getLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            localStorage.setItem("userLocation", JSON.stringify(pos));
+            codeLatLng(pos.lat, pos.lng);
+          }, function() {
+            handleLocationError(true);
+          });
+    } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false);
+    }
+}
+
+function handleLocationError(browserHasGeolocation) {
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
+
+
+var geocoder= new google.maps.Geocoder();
+function codeLatLng(lat, lng) {
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({latLng: latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        var arrAddress = results;
+
+        for(var i = 0; i < arrAddress.length; i++){
+            if(arrAddress[i].types[0] == "locality"){
+                localStorage.setItem("location", arrAddress[i].address_components[0].long_name);
+                document.getElementById("location").textContent = localStorage.getItem("location");
+                break;
+            }
+        }
+
+      } else {
+        alert("No results found");
+      }
+    } else {
+      alert("Geocoder failed due to: " + status);
+    }
+  });
+}
